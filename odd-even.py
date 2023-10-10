cf179235c341fa7fb7840e286512cd25dc2b9635
Ryan Cannan <samchavoya@gmail.com>
2023-10-10 00:07:08 -0700

    odd-even

---
+++ odd-even.py
@@ -0,0 +1,9 @@
+if sam == user:
+    print("helloooooooooooo")
+
+# say you want a function to determine if your variable, n, is odd or even
+def even_odd(n):
+    if n % 2 == 0:
+        print(n, " is an even number")
+    else: 
+        print(n, "is an odd number")
